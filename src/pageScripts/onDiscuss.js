34bbc57c646b40d31aed9b522ceb63843d1f1cfc
LiteHell <LiteHell@openmailbox.org>
2016-01-18 23:28:15 +0900

    개발중
     * 개발중

--- src/pageScripts/onDiscuss.js
+++ src/pageScripts/onDiscuss.js
@@ -1,19 +1,24 @@
 conditionalLoader.register("IsDiscussing", function() {
   var observer = null;
 
-  function colorize() {
-
+  var colorHash = new ColorHash();
+  function beautify(){
+    
   }
   this.load = function() {
+    // 토론 컨테이너 불려오기
     var container = document.querySelector("#res-container");
+    // 없으면 로드 실패
     if (container == null) {
       this.loaded = false;
       return;
     }
     this.loaded = true;
+
+    // 옵저버 선언
     observer = new MutationObserver(function(objs) {
       objs.forEach(function(obj) {
-        console.log(obj.type);
+        var node = obj.addedNodes;
       })
     });
     observer.observe(container, {
